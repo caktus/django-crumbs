55bfbbf25a4bc7ffff4829e0e6dc009394fdb68d
Colin Copeland <copelco@caktusgroup.com>
2009-08-24 14:08:11 +0000

    basic project structure

---
+++ crumbs/templatetags/breadcrumb_tags.py
@@ -0,0 +1,44 @@
+import copy
+import pprint
+
+from django import template
+from django.template.loader import render_to_string
+from django.core.urlresolvers import reverse
+
+from breadcrumbs.templatetags import CaktNode, parse_args_kwargs
+
+
+register = template.Library()
+
+
+class AddCrumbNode(CaktNode):
+    def render_with_args(self, context, crumb, url=None, *args):
+        href = None
+        if url:
+            if '/' in url:
+                href = url
+            else:
+                href = reverse(url, args=args)
+        if not hasattr(context['request'], 'breadcrumbs'):
+            context['request'].breadcrumbs = []
+        context['request'].breadcrumbs.append((crumb, href))
+        return ''
+
+
+@register.tag
+def add_crumb(parser, token):
+    tag_name, args, kwargs = parse_args_kwargs(parser, token)
+    return AddCrumbNode(*args, **kwargs)
+
+
+@register.inclusion_tag('breadcrumbs/crumbs.html', takes_context=True)
+def render_breadcrumbs(context):
+    try:
+        crumbs = context['request'].breadcrumbs
+    except AttributeError:
+        crumbs = None
+    if crumbs and len(crumbs) == 0:
+        crumbs = None
+    return {
+        'crumbs': crumbs,
+    }
