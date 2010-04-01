4cd419a0fe5fcdf0c570570d55426e33a3f12523
Michael Clemmons <mike@caktusgroup.com>
2010-04-01 18:28:56 +0000

    Added add tests

--- crumbs/tests.py
+++ crumbs/tests.py
@@ -19,3 +19,16 @@ class CrumbsTestCase(unittest.TestCase):
         context['request'].breadcrumbs.append(('Test2', '/'))
         render_breadcrumbs(context)
         
+    def test_add_without_request(self):
+        render_breadcrumbs({})
+        
+    def test_add_with_request(self):
+        render_breadcrumbs({'request': ''})
+        
+    def test_add_with_request_and_crumbs(self):
+        context = {}
+        context['request'] = HttpRequest() 
+        context['request'].breadcrumbs = []
+        context['request'].breadcrumbs.append(('Test1', '/'))
+        context['request'].breadcrumbs.append(('Test2', '/'))
+        render_breadcrumbs(context)
