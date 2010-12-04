65f878f33dfe27d3394f05c8fc0b5a79d58e47cc
Colin Copeland <copelco@caktusgroup.com>
2010-12-04 12:16:05 -0500

    prep for pypi release

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
-import os
 from setuptools import setup, find_packages
-
-packages = find_packages()
-packages.remove('sample_project')
 
 setup(
     name='django-crumbs',
     version=__import__('crumbs').__version__,
     author='Caktus Consulting Group',
     author_email='solutions@caktusgroup.com',
-    packages=find_packages(),
-    include_package_data = True,
-    exclude_package_data={
-        '': ['*.sql', '*.pyc',],
-    },
+    include_package_data=True,
+    packages=find_packages(exclude=['sample_project']),
+    exclude_package_data={'': ['*.sql', '*.pyc']},
     url='http://github.com/caktus/django-crumbs/',
-    license='LICENSE.txt',
+    license='BSD',
     description='A pluggable Django app for adding breadcrumbs to your project. ',
     classifiers=[
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
@@ -28,3 +22,4 @@ setup(
     ],
     long_description=open('README.rst').read(),
 )
+
