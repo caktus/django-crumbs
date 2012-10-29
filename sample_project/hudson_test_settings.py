b4c266aa51e5d621a079bf778e5518750645b3cd
Karen Tracey <kmtracey@caktusgroup.com>
2012-10-29 16:50:04 -0400

    Delete sample project.
    It was only  useful for testing, replaced by runtests.py. Refs #2.

--- sample_project/hudson_test_settings.py
+++
@@ -1,6 +0,0 @@
-from sample_project.settings import *
-
-TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.run_tests'
-TEST_OUTPUT_VERBOSE = True
-TEST_OUTPUT_DESCRIPTIONS = True
-TEST_OUTPUT_DIR = 'xmlrunner'
